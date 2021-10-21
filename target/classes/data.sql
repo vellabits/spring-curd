c5cda9ed931efbb751927a53052e87c95060b195
Vellaisamy86 <66508392+Vellaisamy86@users.noreply.github.com>
2021-10-21 23:52:23 +0800

    second commit

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO 
 	TBL_EMPLOYEES (first_name, last_name, email) 
 VALUES
-  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
-  	('John', 'Doe', 'xyz@email.com');
+  	('Vellaisamy', 'S', '2020mt93200@wilp.bits-pilani.ac.in'),
+  	('Deepak', 'VN', '2020MT93148@wilp.bits-pilani.ac.in'),
+  	('Jashvanthi', 'Varu', '2020MT93173@wilp.bits-pilani.ac.in'),
+  	('Anil','Choudhary','2020MT93054@wilp.bits-pilani.ac.in');
